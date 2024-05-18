fff58e47265a04906a42a5c420923d0d8c24c82c
Kirill Toptun <kirilltoptun@gmail.com>
2024-05-18 12:42:51 +1200

    first commit

---
+++ src/main/java/com/example/passwordmanagerapp/Account.java
@@ -0,0 +1,36 @@
+package com.example.passwordmanagerapp;
+
+public class Account {
+
+    String domain;
+    String username;
+    String password;
+
+    public Account(String domain, String username, String password){
+        this.domain = domain;
+        this.username = username;
+        this.password = password;
+    }
+
+    // returns the domain for this account
+    public String getDomain(){
+        return this.domain;
+    }
+    // returns the username for this account
+    public String getUsername(){
+        return this.username;
+    }
+    // returns the password for this account
+    public String getPassword(){
+        return this.password;
+    }
+
+    // changes the domain for this account
+    public void changeDomain(String newDomain){
+        this.domain = newDomain;
+    }
+    // changes the password for this account
+    public void changePassword(String newPassword){
+        this.password = newPassword;
+    }
+}
